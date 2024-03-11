03bdd5d7da54768450b9abc4fedd74a3cb7b4b4d
Uz1i <161866914+Uz1i@users.noreply.github.com>
2024-03-11 17:32:10 +0000

    Extra dec.

--- slowRoot_slow.py
+++ slowRoot_slow.py
@@ -3,7 +3,8 @@ from decimal import Decimal
 num = Decimal(input("number: "))
 
 sqrtNum = Decimal("0")
-
+# Keeps on incrementing the number until
+# it reaches the square root
 while sqrtNum != num/num:
   sqrtNum+=Decimal("0.00001")
 
